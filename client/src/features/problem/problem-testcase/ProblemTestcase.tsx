1f3d64cb5bac2b4f40aaa3d50d6a9a53bc0dd33d
Anthony McLamb <adylanmclamb@yahoo.com>
2024-02-09 14:41:50 -0500

    update unsued var

--- client/src/features/problem/problem-testcase/ProblemTestcase.tsx
+++ client/src/features/problem/problem-testcase/ProblemTestcase.tsx
@@ -1,6 +1,4 @@
-type Props = {};
-
-const ProblemTestcase = (props: Props) => {
+const ProblemTestcase = () => {
   return (
     <div className="p-5">
       <p className="font-semibold">Test Cases are not available</p>
