e70ffba0b17191c7459d596ca0dfc4cadc697561
Anthony McLamb <adylanmclamb@yahoo.com>
2024-03-16 00:13:25 -0400

    adding check for account

--- backend/src/account/account.controller.ts
+++ backend/src/account/account.controller.ts
@@ -1,7 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import { AccountService } from './account.service';
+import { Request } from 'express';
+import { Account } from 'src/data-model/entities';
+import { AccountNotFoundException } from './exceptions/account-not-found.exception';
+import { AuthorizationGuard } from 'src/auth/authorization.guard';
 
-@Controller('account')
+@Controller('/v1/account')
 export class AccountController {
   constructor(private readonly accountService: AccountService) {}
+
+  @UseGuards(AuthorizationGuard)
+  @Get('find/sub')
+  async findAccountBySub(@Req() request: Request): Promise<Account> {
+    const userSub = request.auth.payload.sub;
+    if (!userSub) {
+      throw new AccountNotFoundException();
+    }
+
+    return this.accountService.findAccountBySub(userSub);
+  }
 }
