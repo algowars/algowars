84ac2ef047ec58e31b848f0847ef217fc02921e6
Anthony McLamb <adylanmclamb@yahoo.com>
2024-04-29 21:35:38 -0400

    remove tests

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -20,20 +20,6 @@ const App = () => {
     );
   }
 
-  const allCSS = [...document.styleSheets]
-    .map((styleSheet) => {
-      try {
-        return [...styleSheet.cssRules].map((rule) => rule.cssText).join("");
-      } catch (e) {
-        console.log(
-          "Access to stylesheet %s is denied. Ignoring…",
-          styleSheet.href
-        );
-      }
-    })
-    .filter(Boolean)
-    .join("\n");
-
   console.log(allCSS);
   return (
     <>
