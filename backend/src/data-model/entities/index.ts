235cde72894dd9dccf6b3118c9dffb7cb94289cd
Anthony McLamb <adylanmclamb@yahoo.com>
2024-05-12 16:42:41 -0400

    Revert "update to new erd"
    This reverts commit 2bb9299dd768300591c1dc0d5671bfabee7b8beb.

--- backend/src/data-model/entities/index.ts
+++ backend/src/data-model/entities/index.ts
@@ -1,14 +1,13 @@
 import { RushDuration } from './game/rush/rush-duration.entity';
 import { RushProblem } from './game/rush/rush-problem.entity';
 import { Rush } from './game/rush/rush.entity';
-import { Language } from './language.entity';
 import { Account } from './player/account.entity';
 import { Player } from './player/player.entity';
 import { ProblemSetup } from './problem/problem-setup.entity';
 import { Problem } from './problem/problem.entity';
 import { TestInput } from './problem/test-input.entity';
 import { Test } from './problem/test.entity';
-import { Status } from './submission/status.entity';
+import { SubmissionToken } from './submission/submission-token.entity';
 import { Submission } from './submission/submission.entity';
 
 const entities = [
@@ -18,9 +17,8 @@ const entities = [
   ProblemSetup,
   Test,
   TestInput,
-  Language,
   Submission,
-  Status,
+  SubmissionToken,
   Rush,
   RushProblem,
   RushDuration,
@@ -34,8 +32,7 @@ export {
   Test,
   TestInput,
   Submission,
-  Language,
-  Status,
+  SubmissionToken,
   Rush,
   RushProblem,
   RushDuration,
--- backend/src/data-model/entities/submission/submission.entity.ts
+++ backend/src/data-model/entities/submission/submission.entity.ts
@@ -3,37 +3,36 @@ import {
   CreateDateColumn,
   Entity,
   ManyToOne,
+  OneToMany,
   PrimaryGeneratedColumn,
   UpdateDateColumn,
 } from 'typeorm';
 import { Player } from '../player/player.entity';
 import { Problem } from '../problem/problem.entity';
-import { Language } from '../language.entity';
-import { Status } from './status.entity';
+import { SubmissionToken } from './submission-token.entity';
 
 @Entity()
 export class Submission {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
-  @Column('text', { nullable: false })
-  sourceCode: string;
+  @OneToMany(
+    () => SubmissionToken,
+    (submissionToken) => submissionToken.submission,
+    {
+      cascade: true,
+    },
+  )
+  tokens: SubmissionToken[];
 
-  @ManyToOne(() => Language, { nullable: false })
-  language: Language;
-
-  @Column('text', { nullable: true })
-  stdout: string;
-
-  @Column({
-    type: 'enum',
-    enum: Status,
-    default: Status.IN_QUEUE,
-  })
-  status: Status;
+  @Column({ nullable: false })
+  code: string;
 
   @ManyToOne(() => Player, (player) => player.submissions)
   createdBy: Player;
+
+  @Column({ nullable: false })
+  languageId: number;
 
   @ManyToOne(() => Problem, (problem) => problem.submissions)
   problem: Problem;
@@ -43,4 +42,8 @@ export class Submission {
 
   @UpdateDateColumn({ type: 'timestamptz', default: () => 'CURRENT_TIMESTAMP' })
   updatedAt: Date;
+
+  public getTokens(): string[] {
+    return this.tokens.map((token) => token.token);
+  }
 }
