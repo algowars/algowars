92eaeaf24ad7e8014854ec98cc73e8aec9bc703e
Anthony McLamb <adylanmclamb@yahoo.com>
2024-05-18 03:13:50 -0400

    setting up mocks

---
+++ backend/src/problem/mocks/problem.mock.ts
@@ -0,0 +1,47 @@
+import { PaginationResponse } from 'src/common/pagination/dtos/pagination-response.dto';
+import { ProblemPaginationDto } from '../dtos/problem-pagination.dto';
+import { Player, Problem } from 'src/data-model/entities';
+import { CreateProblemDto } from '../dtos/create-problem.dto';
+import { QueryOptions } from 'src/common/query/query-options';
+
+export class ProblemMock {
+  static mockProblemService(problems: Problem[], totalPages = 1) {
+    return {
+      getProblemsPageable: jest.fn(
+        (dto: ProblemPaginationDto): Promise<PaginationResponse<Problem>> => {
+          const mockProblems: Problem[] = problems;
+          return Promise.resolve({
+            results: mockProblems,
+            page: dto.page,
+            size: dto.size,
+            totalPages,
+          });
+        },
+      ),
+      create: jest.fn(
+        (
+          { title, question, slug }: CreateProblemDto,
+          player: Player,
+        ): Promise<Problem> => {
+          const problem = new Problem();
+
+          problem.title = title;
+          problem.question = question;
+          problem.slug = slug;
+          problem.createdBy = Promise.resolve(player);
+
+          return Promise.resolve(problem);
+        },
+      ),
+      findOneById: jest.fn((id: number) => {
+        return problems.find((p) => p.id === id);
+      }),
+      findProblemWithTests: jest.fn((id: number) => {
+        return problems.find((p) => p.id === id);
+      }),
+      findOneBySlug: jest.fn((slug: string) => {}),
+      findRandomProblem: jest.fn(() => {}),
+      findProblemsByRating: jest.fn(() => {}),
+    };
+  }
+}
