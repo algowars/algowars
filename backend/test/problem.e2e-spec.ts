b0b3dca2dc7cc60719d75e9721479e27f219d002
Anthony McLamb <adylanmclamb@yahoo.com>
2024-06-01 23:33:41 -0400

    setting up e2e

--- backend/test/problem.e2e-spec.ts
+++ backend/test/problem.e2e-spec.ts
@@ -3,7 +3,7 @@ import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 
-describe('AppController (e2e)', () => {
+describe('ProblemController (e2e)', () => {
   let app: INestApplication;
 
   beforeEach(async () => {
@@ -15,10 +15,15 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
+  // get problems pageable
   it('/ (GET)', () => {
     return request(app.getHttpServer())
-      .get('/')
+      .get('/v1/problem')
       .expect(200)
       .expect('Hello World!');
   });
+
+  // create problems
+
+  // update problem
 });
