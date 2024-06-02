568e981ca6f667abf1e1bc2b50a825119dd5692f
Anthony McLamb <adylanmclamb@yahoo.com>
2024-06-01 23:58:16 -0400

    back to old client

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -1,5 +1,31 @@
+import { useAuth0 } from "@auth0/auth0-react";
+import PageLoader from "./components/loader/page-loader/page-loader";
+import PageRoutes from "./pages/page-routes";
+import { useAccount } from "./features/account/use-account";
+import { useAuthRoles } from "./features/auth/auth-roles/use-auth-roles";
+import { Toaster } from "./components/ui/sonner";
+
 const App = () => {
-  return <div>App</div>;
+  const { isLoading } = useAuth0();
+
+  const { isLoading: isAccountLoading } = useAccount();
+
+  useAuthRoles();
+
+  if (isLoading || isAccountLoading) {
+    return (
+      <div>
+        <PageLoader />
+      </div>
+    );
+  }
+
+  return (
+    <>
+      <PageRoutes />
+      <Toaster />
+    </>
+  );
 };
 
 export default App;
