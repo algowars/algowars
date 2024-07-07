6397518cb67f0fbd68f4227f4b927c492597c7b8
ZhiZheng0889 <zhizheng33782@gmail.com>
2024-07-07 19:52:06 -0400

    Just commenting the codes and creating a readme file for the command folder in backend for documentation.

--- backend/src/account/commands/create-account/create-account.command.ts
+++ backend/src/account/commands/create-account/create-account.command.ts
@@ -1,8 +1,12 @@
 import { CreateAccountRequest } from 'src/account/dto/request/create-account-request.dto';
 
+// Define the CreateAccountCommand class
 export class CreateAccountCommand {
+  // Constructor to initialize the CreateAccountCommand object
   constructor(
+    // The createAccountRequest parameter of type CreateAccountRequest
     public readonly createAccountRequest: CreateAccountRequest,
+    // The sub parameter of type string
     public readonly sub: string,
-  ) {}
+  ) { }
 }
--- backend/src/account/commands/create-account/create-account.handler.ts
+++ backend/src/account/commands/create-account/create-account.handler.ts
@@ -2,26 +2,29 @@ import { CommandHandler, EventPublisher, ICommandHandler } from '@nestjs/cqrs';
 import { CreateAccountCommand } from './create-account.command';
 import { AccountFactory } from 'src/account/account.factory';
 
+// Define the CreateAccountHandler class as a Command Handler for CreateAccountCommand
 @CommandHandler(CreateAccountCommand)
 export class CreateAccountHandler
-  implements ICommandHandler<CreateAccountCommand>
-{
+  implements ICommandHandler<CreateAccountCommand> {
+  // Constructor to initialize dependencies
   constructor(
-    private readonly accountFactory: AccountFactory,
-    private readonly eventPublisher: EventPublisher,
-  ) {}
+    private readonly accountFactory: AccountFactory, // AccountFactory dependency
+    private readonly eventPublisher: EventPublisher, // EventPublisher dependency
+  ) { }
 
+  // Execute method to handle the command
   async execute({
     createAccountRequest,
     sub,
   }: CreateAccountCommand): Promise<string> {
-    const { username } = createAccountRequest;
+    const { username } = createAccountRequest; // Extract username from the request
 
+    // Create an account and merge it with the event context
     const account = this.eventPublisher.mergeObjectContext(
       await this.accountFactory.create(sub, username),
     );
 
-    account.commit();
-    return account.getId();
+    account.commit(); // Commit the account creation event
+    return account.getId(); // Return the account ID
   }
 }
