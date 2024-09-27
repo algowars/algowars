755fb31f929be6c42542542dac0b6497f7fdd272
Anthony Mclamb <adylanmclamb@yahoo.com>
2024-09-27 10:27:02 -0400

    add component

--- client/src/components/auth/protected-route/protected-route.test.tsx
+++ client/src/components/auth/protected-route/protected-route.test.tsx
@@ -1,8 +1,11 @@
 import { render } from "@testing-library/react";
 import { ProtectedRoute } from "./protected-route";
 
+const TestComponent = () => {
+  return <h1>TESTING</h1>;
+};
 describe("ProtectedRoute", () => {
-    test("should be defined", () => {
-        render(<ProtectedRoute />)
-    })
-})
+  test("should be defined", () => {
+    render(<ProtectedRoute component={TestComponent} />);
+  });
+});
