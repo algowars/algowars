85392aa13ff4d3068b0ae4692ca4ce3eed955303
Anthony Mclamb <adylanmclamb@yahoo.com>
2024-09-28 14:17:32 -0400

    update env file

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -8,7 +8,7 @@ const config: Config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://your-docusaurus-site.example.com",
+  url: process.env.URL,
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
@@ -91,7 +91,7 @@ const config: Config = {
           items: [
             {
               label: "Tutorial",
-              to: "/docs/intro",
+              to: "/docs/introduction",
             },
           ],
         },
