2a2bfbac44f539689d66bad6a35d68ab00588ec6
Anthony Mclamb <adylanmclamb@yahoo.com>
2024-10-05 01:36:24 -0400

    fix tests

--- backend/src/account/domain/account.spec.ts
+++ backend/src/account/domain/account.spec.ts
@@ -1,13 +1,15 @@
 import { Id, IdImplementation } from 'src/common/domain/id';
 import { AccountImplementation } from './account';
+import { UsernameImplementation } from './username';
+import { UserSubImplementation } from './user-sub';
 
 describe('Account', () => {
   it('should compare id', () => {
     const expectedId = 'id';
     const account = new AccountImplementation({
       id: new IdImplementation(expectedId) as Id,
-      sub: 'auth|lk00-089aj8jdf-aj98fja',
-      username: 'test_username',
+      sub: new UserSubImplementation('auth|lk00-089aj8jdf-aj98fja'),
+      username: new UsernameImplementation('test_username'),
       createdAt: new Date(),
       updatedAt: new Date(),
       deletedAt: null,
--- backend/src/problem/domain/problem.spec.ts
+++ backend/src/problem/domain/problem.spec.ts
@@ -1,5 +1,8 @@
 import { IdImplementation } from 'src/common/domain/id';
 import { ProblemImplementation } from './problem';
+import { AccountImplementation } from 'src/account/domain/account';
+import { UserSubImplementation } from 'src/account/domain/user-sub';
+import { UsernameImplementation } from 'src/account/domain/username';
 
 describe('Problem', () => {
   it('should compare id', () => {
@@ -13,6 +16,15 @@ describe('Problem', () => {
       updatedAt: new Date(),
       deletedAt: null,
       version: 1,
+      createdBy: new AccountImplementation({
+        id: new IdImplementation('test-id'),
+        sub: new UserSubImplementation('github|1231-12312-123123-123'),
+        username: new UsernameImplementation('USERNAME'),
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        deletedAt: null,
+        version: 1,
+      }),
     });
 
     expect(problem.compareId(expectedId)).toBeTruthy();
