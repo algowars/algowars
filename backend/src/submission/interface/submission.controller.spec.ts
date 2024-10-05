0e267636cf6c6f6b5f5aae4f80979dea5a0dcba6
Anthony Mclamb <adylanmclamb@yahoo.com>
2024-10-04 23:10:36 -0400

    update test

--- backend/src/submission/interface/submission.controller.spec.ts
+++ backend/src/submission/interface/submission.controller.spec.ts
@@ -1,17 +1,33 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { SubmissionController } from './submission.controller';
-import { SubmissionService } from './submission.service';
+import { CommandBus } from '@nestjs/cqrs';
+import { AuthorizationGuard } from 'src/auth/authorization.guard';
+import { AccountAuthorizationGuard } from 'src/auth/account-authorization.guard';
 
 describe('SubmissionController', () => {
   let controller: SubmissionController;
+  let commandBus: CommandBus;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [SubmissionController],
-      providers: [SubmissionService],
-    }).compile();
+      providers: [
+        {
+          provide: CommandBus,
+          useValue: {
+            execute: jest.fn(),
+          },
+        },
+      ],
+    })
+      .overrideGuard(AuthorizationGuard)
+      .useValue({ canActivate: jest.fn(() => true) })
+      .overrideGuard(AccountAuthorizationGuard)
+      .useValue({ canActivate: jest.fn(() => true) })
+      .compile();
 
     controller = module.get<SubmissionController>(SubmissionController);
+    commandBus = module.get<CommandBus>(CommandBus);
   });
 
   it('should be defined', () => {
