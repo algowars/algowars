b9e6719827d7bf565ea721b76ba60686dcab67a3
Anthony Mclamb <adylanmclamb@yahoo.com>
2024-11-10 17:35:30 -0500

    temp remove extra window

--- client/src/features/problem/problem-editor/problem-editor.tsx
+++ client/src/features/problem/problem-editor/problem-editor.tsx
@@ -67,7 +67,7 @@ export const ProblemEditor = ({ problem }: ProblemEditorProps) => {
           <ResizableHandle className="p-2 bg-inherit hover:bg-muted" />
           <ResizablePanel defaultSize={45} minSize={10}>
             <ResizablePanelGroup direction="vertical">
-              <ResizablePanel defaultSize={80} minSize={15}>
+              <ResizablePanel defaultSize={100} minSize={15}>
                 <Card className="h-full">
                   <div className="p-5 border-b">
                     <h2 className="text-2xl font-semibold">{problem.title}</h2>
@@ -76,8 +76,8 @@ export const ProblemEditor = ({ problem }: ProblemEditorProps) => {
                   <div className="p-5">{problem.question}</div>
                 </Card>
               </ResizablePanel>
-              <ResizableHandle className="p-3 bg-inherit hover:bg-muted" />
-              <ResizablePanel defaultSize={20} minSize={15}>
+              {/* <ResizableHandle className="p-3 bg-inherit hover:bg-muted" /> */}
+              <ResizablePanel defaultSize={0}>
                 <Card className="h-full">
                   <div className="p-5 border-b">
                     <h2 className="text-2xl font-semibold">{problem.title}</h2>
