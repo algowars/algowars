d932502a867b2a2fd9a18e6e920aa6510b0bfb61
Anthony Mclamb <adylanmclamb@yahoo.com>
2024-12-28 00:25:18 -0500

    hopeful fix

--- backend/knexfile.ts
+++ backend/knexfile.ts
@@ -2,11 +2,11 @@ import 'dotenv/config';
 import * as path from 'path';
 
 const {
-  DATABASE_HOST = 'localhost',
-  DATABASE_PORT = 5432,
-  DATABASE_USER = 'postgres',
-  DATABASE_PASSWORD = 'postgres',
-  DATABASE_NAME = 'postgres',
+  DATABASE_HOST,
+  DATABASE_PORT,
+  DATABASE_USER,
+  DATABASE_PASSWORD,
+  DATABASE_NAME,
   DEBUG,
 } = process.env;
 
