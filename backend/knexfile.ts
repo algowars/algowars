0c08505026627456bc6553e5de8a1747d5e7da79
Anthony Mclamb <adylanmclamb@yahoo.com>
2024-12-27 23:50:16 -0500

    remove fix

--- backend/knexfile.ts
+++ backend/knexfile.ts
@@ -29,24 +29,6 @@ const config = {
     },
     debug: !!DEBUG,
   },
-  production: {
-    client: 'pg',
-    pool: { min: 1, max: 5 },
-    connection: {
-      database: DATABASE_NAME,
-      user: DATABASE_USER,
-      password: DATABASE_PASSWORD,
-      host: DATABASE_HOST,
-      port: DATABASE_PORT,
-    },
-    migrations: {
-      directory: path.resolve(__dirname, 'src', 'db', 'migrations'),
-    },
-    seeds: {
-      directory: path.resolve(__dirname, 'src', 'db', 'seeds'),
-    },
-    debug: !!DEBUG,
-  },
 };
 
 export default config;
