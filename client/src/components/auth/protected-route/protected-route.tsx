675cefc5476761654708949fa55a0d5668a8a5f4
admclamb <adylanmclamb@yahoo.com>
2025-01-03 00:19:55 -0500

    remove attempt to fix auth bug

--- client/src/components/auth/protected-route/protected-route.tsx
+++ client/src/components/auth/protected-route/protected-route.tsx
@@ -2,22 +2,16 @@ import { PageLoader } from "@/components/loader/page-loader/page-loader";
 import { useAccountStore } from "@/features/account/account-store.provider";
 import { withAuthenticationRequired } from "@auth0/auth0-react";
 import { ComponentType } from "react";
-import { useNavigate } from "react-router-dom";
 
 export type ProtectedRouteProps = {
   component: ComponentType<object>;
 };
 
 export const ProtectedRoute = ({ component }: ProtectedRouteProps) => {
-  const { isLoading, isAuthenticated } = useAccountStore();
-  const navigate = useNavigate();
+  const { isLoading } = useAccountStore();
 
   if (isLoading) {
     return <PageLoader />;
-  }
-
-  if (!isLoading && !isAuthenticated) {
-    return navigate(-1);
   }
 
   const Component = withAuthenticationRequired(component, {
