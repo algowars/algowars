2f780e4d10006b091a0a9eea1728b014cc1de586
admclamb <adylanmclamb@yahoo.com>
2025-01-17 13:08:29 -0500

    add alpha logo

--- client/src/components/logos/logo/logo.tsx
+++ client/src/components/logos/logo/logo.tsx
@@ -1,5 +1,6 @@
 import { cn } from "@/lib/utils";
 import logo192 from "/logo192.png";
+import { Badge } from "@/components/ui/badge";
 
 export type LogoProps = {
   imgClassName?: string;
@@ -8,9 +9,12 @@ export type LogoProps = {
 
 export const Logo = ({ imgClassName, className }: LogoProps) => {
   return (
-    <span className={cn("flex items-center", className)}>
+    <span className={cn("flex items-center relative", className)}>
       <img src={logo192} className={cn("h-8 mr-2", imgClassName)} />
       <h1 className="text-xl font-semibold">Algowars</h1>
+      <Badge className="text-xs px-1.5 py-0.25 absolute -right-12 -top-0">
+        Alpha
+      </Badge>
     </span>
   );
 };
