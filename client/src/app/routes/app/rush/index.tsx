97515cb5d445017554f58e06ffccc34ddb11ae14
admclamb <adylanmclamb@yahoo.com>
2025-02-07 14:53:03 -0500

    update dev cli and rix breadcrumbs bug

--- client/src/app/routes/app/rush/index.tsx
+++ client/src/app/routes/app/rush/index.tsx
@@ -1,3 +1,4 @@
+import { routerConfig } from "@/app/router-config";
 import { SidebarLayout } from "@/components/layouts/sidebar-layout/sidebar-layout";
 import { PageLoader } from "@/components/loader/page-loader/page-loader";
 import { Suspense } from "react";
@@ -11,7 +12,19 @@ export const RushRoute = () => {
           key={location.pathname}
           fallback={<div>Something went wrong!</div>}
         >
-          <SidebarLayout className="flex flex-col h-[1px]">
+          <SidebarLayout
+            className="flex flex-col h-[1px]"
+            breadcrumbs={[
+              {
+                href: routerConfig.root.path,
+                name: "Home",
+              },
+              {
+                href: routerConfig.rush.path,
+                name: "Rush",
+              },
+            ]}
+          >
             <h1>RUSH GAMEMODE</h1>
           </SidebarLayout>
         </ErrorBoundary>
