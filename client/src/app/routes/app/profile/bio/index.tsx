e3326f5498f47e173b46e779217affc6c93187b2
admclamb <adylanmclamb@yahoo.com>
2025-02-09 12:36:26 -0500

    fix ui

--- client/src/app/routes/app/profile/bio/index.tsx
+++ client/src/app/routes/app/profile/bio/index.tsx
@@ -5,6 +5,7 @@ import { useParams } from "react-router-dom";
 import dayjs from "dayjs";
 import { UserSubmissions } from "@/features/submission/user-submissions/user-submissions";
 import { SidebarLayout } from "@/components/layouts/sidebar-layout/sidebar-layout";
+import { routerConfig } from "@/app/router-config";
 
 export const ProfileBioRoute = () => {
   const { username } = useParams();
@@ -28,19 +29,32 @@ export const ProfileBioRoute = () => {
   }
 
   return (
-    <SidebarLayout breadcrumbs={[]}>
-      <Card className="p-5 flex flex-col gap-5 bg-sidebar mb-5">
-        <h2 className="font-bold text-2xl">{account.username}</h2>
-        {/* {profileRank ? <PlayerRankBadge elo={profileRank.elo} /> : null} */}
+    <SidebarLayout
+      breadcrumbs={[
+        {
+          href: routerConfig.root.path,
+          name: "Home",
+        },
+        {
+          href: routerConfig.profile.path,
+          name: `${username}'s Profile`,
+        },
+      ]}
+    >
+      <section className="px-3">
+        <Card className="p-5 flex flex-col gap-5 bg-sidebar mb-5">
+          <h2 className="font-bold text-2xl">{account.username}</h2>
+          {/* {profileRank ? <PlayerRankBadge elo={profileRank.elo} /> : null} */}
 
-        <p className="text-muted-foreground font-semibold">
-          Created on {dayjs(account.createdAt).format("DD/MM/YYYY")}
-        </p>
-      </Card>
-      <Card className="p-5  bg-sidebar">
-        <h3 className="text-xl font-bold mb-2">Recent Submissions</h3>
-        <UserSubmissions submissions={account.recentSubmissions} />
-      </Card>
+          <p className="text-muted-foreground font-semibold">
+            Created on {dayjs(account.createdAt).format("DD/MM/YYYY")}
+          </p>
+        </Card>
+        <Card className="p-5  bg-sidebar">
+          <h3 className="text-xl font-bold mb-2">Recent Submissions</h3>
+          <UserSubmissions submissions={account.recentSubmissions} />
+        </Card>
+      </section>
     </SidebarLayout>
   );
 };
