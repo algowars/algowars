7060ea01079efddc3fea5306c49b06fd056a1472
admclamb <adylanmclamb@yahoo.com>
2025-02-11 01:26:31 -0500

    problem table enhancements

---
+++ client/src/features/problem/problems-table/problem-tables-columns.tsx
@@ -0,0 +1,53 @@
+import { ColumnDef } from "@tanstack/react-table";
+import { Problem } from "../models/problem.model";
+import { DifficultyBadge } from "@/components/difficulty-badge/difficulty-badge";
+import { Button } from "@/components/ui/button";
+import { ArrowUpDown } from "lucide-react";
+
+export const columns: ColumnDef<Problem>[] = [
+  {
+    accessorKey: "title",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Title
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+  },
+  {
+    accessorKey: "tags",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Tags
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+  },
+  {
+    accessorKey: "difficulty",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Difficulty
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+    cell: ({ row }) => {
+      return <DifficultyBadge difficulty={row.getValue("difficulty")} />;
+    },
+  },
+];
--- client/src/features/problem/problems-table/problems-table.tsx
+++ client/src/features/problem/problems-table/problems-table.tsx
@@ -1,24 +1,13 @@
 import { usePagination } from "@/components/pagination/pagination-context.provider";
-import { PaginationFooter } from "@/components/pagination/pagination-footer";
 import { useGetProblems } from "../api/get-problems-paginated";
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
-import { useNavigate } from "react-router-dom";
-import { DifficultyBadge } from "@/components/difficulty-badge/difficulty-badge";
-import { routerConfig } from "@/app/router-config";
 import { Loader } from "@/components/loader/loader";
 import { useEffect } from "react";
 import { toast } from "sonner";
 import { isAxiosError } from "axios";
+import { ProblemDataTable } from "./problem-data-table";
+import { columns } from "./problem-tables-columns";
 
 export const ProblemsTable = () => {
-  const navigate = useNavigate();
   const { page, size, timestamp } = usePagination();
 
   const problemsQuery = useGetProblems({ page, size, timestamp });
@@ -46,46 +35,11 @@ export const ProblemsTable = () => {
   }
 
   return (
-    <>
-      <Table className="min-w-[37.5rem] mb-3">
-        <TableHeader>
-          <TableRow>
-            <TableHead>Title</TableHead>
-            <TableHead>Tags</TableHead>
-            <TableHead>Difficulty</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {!problemsQuery.data ? (
-            <TableRow>
-              <span className="py-3 text-center block">
-                No Problems Available
-              </span>
-            </TableRow>
-          ) : null}
-          {problemsQuery.data?.results.map((problem) => (
-            <TableRow
-              key={problem.id}
-              onClick={() =>
-                navigate(routerConfig.problem.execute(problem.slug))
-              }
-              className="hover:cursor-pointer"
-            >
-              <TableCell>{problem.title}</TableCell>
-              <TableCell className="text-muted-foreground">
-                {problem?.tags?.join(", ")}
-              </TableCell>
-              <TableCell>
-                <DifficultyBadge difficulty={problem.difficulty} />
-              </TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-      <PaginationFooter
-        className="w-full"
-        totalPages={problemsQuery.data?.totalPages ?? 0}
+    <div className="min-w-[37.5rem] mb-3 flex flex-col gap-5">
+      <ProblemDataTable
+        columns={columns}
+        data={problemsQuery?.data?.results ?? []}
       />
-    </>
+    </div>
   );
 };
